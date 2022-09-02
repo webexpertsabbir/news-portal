e5041699997ef2d56743a00215050f6e26c05b40
Sabbir <webexpertsabbir@gmail.com>
2022-09-03 02:54:52 +0600

    news langht add

--- js/app.js
+++ js/app.js
@@ -1,17 +1,17 @@
-const loadCatagori = async() =>{
+const loadCatagori = async () => {
     const url = 'https://openapi.programming-hero.com/api/news/categories';
     const res = await fetch(url);
     const data = await res.json();
     dispalyCatagori(data.data.news_category);
 }
 
-const dispalyCatagori = catagoris =>{
+const dispalyCatagori = catagoris => {
     // console.log(catagoris)
     const catagorilist = document.getElementById('catagori-list');
     catagoris.forEach(catagori => {
         // console.log(catagori);
         const li = document.createElement('li');
-        li.classList.add('cursor-pointer');
+        li.classList.add('cursor-pointer', 'mx-2');
         li.innerHTML = `<a onclick="loadNews(${catagori.category_id})">${catagori.category_name}</a>`;
         catagorilist.appendChild(li);
 
@@ -20,8 +20,8 @@ const dispalyCatagori = catagoris =>{
 
 loadCatagori();
 
-const loadNews = async(category_id) =>{
-    // console.log(news)
+const loadNews = async (category_id) => {
+    // console.log(category_id);
     const url = `https://openapi.programming-hero.com/api/news/category/0${category_id}`;
     // console.log(url)
     const res = await fetch(url);
@@ -29,12 +29,16 @@ const loadNews = async(category_id) =>{
     displayNews(data.data);
 };
 
-const displayNews = blogPost =>{
+const displayNews = blogPost => {
     // console.log(blogPost);
+ 
+    const showLenght = document.getElementById('show-length');
+    showLenght.innerText = blogPost.length;
+    
     const displayNews = document.getElementById('dispaly-news');
     displayNews.textContent = '';
-    blogPost.forEach(news =>{
-        console.log(news);
+    blogPost.forEach(news => {
+        // console.log(news);
         const article = document.createElement('article');
         article.innerHTML = `
         <div class="row py-5">
@@ -67,8 +71,9 @@ const displayNews = blogPost =>{
                     <i class="fa-solid fa-star-half-stroke"></i>
                     </div>
                   </div>
-                  <div class="col d-flex justify-content-end align-items-center  ">
-                    <button  class="btn btn-outline-primary d-block "><i class="fa-sharp fa-solid fa-arrow-right"></i></button>
+                
+                  <div class="col d-flex justify-content-end align-items-center">
+                  <button onclick="loadModalNewss('${news._id}')" class="btn btn-outline-primary d-block"><i class="fa-sharp fa-solid fa-arrow-right"></i> hello</button>
                   </div>
                 </div>
               </div>
@@ -83,3 +88,15 @@ const displayNews = blogPost =>{
 };
 
 loadNews(1);
+
+
+const loadModalNewss = async(id) => {
+    const url = `https://openapi.programming-hero.com/api/news/${id}`;
+    const res = await fetch(url);
+    const data = await res.json();
+    console.log(data)
+}
+
+
+
+
